6a1b50e2b17581d39113e1030d6264efca124649
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-06-20 17:23:22 +0900

    add comic

---
+++ mash/js/comic.js
@@ -0,0 +1,47 @@
+
+let buttonX = 100, buttonY = 650, buttonWidth = 250, buttonHeihgt = 50;
+let bought = false;
+let img;
+
+let cnt = 0;
+const MAX_cnt = 50;
+let time;
+function setup() {
+    createCanvas(windowHeight, windowWidth);
+    img = loadImage('https://raw.githubusercontent.com/yoshigai-web/Reinventing-the-Button/main/img/comic.jpg');
+}
+function draw() {
+    background(255);
+    if(cnt>=MAX_cnt){
+        bought=true;
+        cnt=0;
+    }
+    if (bought) {
+        fill(0);
+        textSize(30);
+        text("Thank you for shopping!", 100, 400);
+        textSize(20);
+        text("Click to back.", 200, 450);
+    } else {
+        image(img, 75, 50);
+        fill(0);
+        textSize(20);
+        text("￥440", 190, 600);
+        // button
+        fill(78, 212, 249, map(cnt, 0, MAX_cnt, 0, 255));
+        rect(buttonX, buttonY, buttonWidth, buttonHeihgt, 5);
+        fill("#322F20");
+        rect(buttonX, buttonY+buttonHeihgt-5, map(cnt, 0, MAX_cnt, 0, buttonWidth), 5, 5);
+        fill(0);
+        textSize(20);
+        text("購入", 180 + 20, buttonY + 30);
+    }
+    if(millis()-time>500 && frameCount%50==0 &&cnt>0)cnt--;
+}
+function mousePressed() {
+    if (buttonX < mouseX && mouseX<buttonX + buttonWidth && buttonY < mouseY && mouseY < buttonY + buttonHeihgt) {
+        cnt++;
+        time=millis();
+    }
+    if (bought) bought = false;
+}
