c736a5ce623540a8c141c98383470ec07f528d8f
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-06-21 18:49:39 +0900

    correct variable name

--- mobile/js/mash_twoButtons.js
+++ mobile/js/mash_twoButtons.js
@@ -1,6 +1,6 @@
-// let buttonX = 80, buttonY = 250, buttonWidth = 200, buttonHeihgt = 100;
+// let buttonX = 80, buttonY = 250, buttonWidth = 200, buttonHeight = 100;
 let buttonNum=2;
-let buttonX = [80, 210], buttonY = [350, 350], buttonWidth = 70, buttonHeihgt = 100, isPressed=[false, false];
+let buttonX = [80, 210], buttonY = [350, 350], buttonWidth = 70, buttonHeight = 100, isPressed=[false, false];
 let bought = false;
 let img;
 
@@ -38,9 +38,9 @@ function draw() {
         for(let i=0; i<buttonNum; i++){
             if(isPressed[i])fill(78, 212, 249);
             else fill(255);
-            rect(buttonX[i], buttonY[i], buttonWidth, buttonHeihgt, 5);
+            rect(buttonX[i], buttonY[i], buttonWidth, buttonHeight, 5);
             fill(216, 128);
-            rect(buttonX[i], buttonY[i]+buttonHeihgt, buttonWidth, -map(cnt, 0, MAX_cnt, 0, buttonWidth), 5, 5);
+            rect(buttonX[i], buttonY[i]+buttonHeight, buttonWidth, -map(cnt, 0, MAX_cnt, 0, buttonHeight), 5, 5);
         }
     }
     if(millis()-time>500 && frameCount%50==0 &&cnt>0)cnt--;
@@ -59,7 +59,7 @@ function checkButtons(){
     for(let i=0; i<buttonNum; i++)isPressed[i]=false;
     for(let i=0; i<touches.length; i++){
         for(let j=0; j<buttonNum; j++){
-            if (buttonX[j] < touches[i].x && touches[i].x<buttonX[j] + buttonWidth && buttonY[j] < touches[i].y && touches[i].y < buttonY[j] + buttonHeihgt) {
+            if (buttonX[j] < touches[i].x && touches[i].x<buttonX[j] + buttonWidth && buttonY[j] < touches[i].y && touches[i].y < buttonY[j] + buttonHeight) {
                 isPressed[j]=true;
                 cnt++;
                 time=millis();
