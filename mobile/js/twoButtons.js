1ecb0a27ad30b07d711339d7027e1a96f0c6f48d
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-06-21 13:48:10 +0900

    change button size

--- mobile/js/twoButtons.js
+++ mobile/js/twoButtons.js
@@ -1,6 +1,6 @@
 // let buttonX = 80, buttonY = 250, buttonWidth = 200, buttonHeihgt = 100;
 let buttonNum=2;
-let buttonX = [90, 220], buttonY = [250, 250], buttonWidth = 50, buttonHeihgt = 50, isPressed=[false, false];
+let buttonX = [80, 210], buttonY = [250, 250], buttonWidth = 70, buttonHeihgt = 100, isPressed=[false, false];
 let bought = false;
 let img;
 
