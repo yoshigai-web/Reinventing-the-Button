731395b6422e89d187df59b2f67bffaa9fd357ad
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-06-26 16:25:19 +0900

    Update forceTouch.js

--- mobile/js/forceTouch.js
+++ mobile/js/forceTouch.js
@@ -1,24 +1,27 @@
-let force=0;
+let force = 0;
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    document.body.addEventListener("touchstart", handle_touch);
-        document.body.addEventListener("touchmove", handle_touch);
+    // document.body.addEventListener("touchstart", handle_touch);
+    // document.body.addEventListener("touchmove", handle_touch);
+    document.body.addEventListener('pointerdown', function (event) {
+        force=event.pressure;
+    }, false);
 }
 function draw() {
     background(255);
     for (let i = 0; i < touches.length; i++) {
         fill(216);
-        ellipse(touches[i].x, touches[i].y, 200*force);
+        ellipse(touches[i].x, touches[i].y, 200 * force);
     }
-}
-function touchStarted() {
-    
+    // if(mouseIsPressed){
+    //     ellipse(mouseX, mouseY, 200*force);
+    // }
 }
 
-function handle_touch(event) {
-    try {
-        force=event.touches[0].force;
-    } catch (e) {
-        console.log("error");
-    }
-}
+// function handle_touch(event) {
+//     try {
+//         force = event.touches[0].force;
+//     } catch (e) {
+//         console.log("error");
+//     }
+// }
