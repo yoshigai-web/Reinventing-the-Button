20164e3e693d3b959d5fc4e9ee07b36dfc60d629
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-06-26 16:51:20 +0900

    Update forceTouch.js

--- mobile/js/forceTouch.js
+++ mobile/js/forceTouch.js
@@ -1,14 +1,15 @@
 let force = 0;
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    // document.body.addEventListener("touchstart", handle_touch);
     document.body.addEventListener('touchstart', function(e) {
-        // Iterate through the list of touch points and log each touch
-        // point's force.
         for (var i=0; i < e.targetTouches.length; i++) {
-          // Add code to "switch" based on the force value. For example
-          // minimum pressure vs. maximum pressure could result in
-          // different handling of the user's input.
+          force=e.targetTouches[i].force;
+          console.log("targetTouches[" + i + "].force = " + e.targetTouches[i].force);
+        }
+     }, false);
+     
+     document.body.addEventListener('touchmove', function(e) {
+        for (var i=0; i < e.targetTouches.length; i++) {
           force=e.targetTouches[i].force;
           console.log("targetTouches[" + i + "].force = " + e.targetTouches[i].force);
         }
