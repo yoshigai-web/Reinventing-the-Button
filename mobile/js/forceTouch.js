1b49ab6590d3d69bcbdceee8ac9b15a1969201bb
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-06-26 16:45:46 +0900

    Update forceTouch.js

--- mobile/js/forceTouch.js
+++ mobile/js/forceTouch.js
@@ -2,10 +2,17 @@ let force = 0;
 function setup() {
     createCanvas(windowWidth, windowHeight);
     // document.body.addEventListener("touchstart", handle_touch);
-    // document.body.addEventListener("touchmove", handle_touch);
-    document.body.addEventListener('pointerdown', function (event) {
-        force=event.pressure;
-    }, false);
+    document.body.addEventListener('touchstart', function(e) {
+        // Iterate through the list of touch points and log each touch
+        // point's force.
+        for (var i=0; i < e.targetTouches.length; i++) {
+          // Add code to "switch" based on the force value. For example
+          // minimum pressure vs. maximum pressure could result in
+          // different handling of the user's input.
+          force=e.targetTouches[i].force;
+          console.log("targetTouches[" + i + "].force = " + e.targetTouches[i].force);
+        }
+     }, false);
 }
 function draw() {
     background(255);
@@ -13,15 +20,4 @@ function draw() {
         fill(216);
         ellipse(touches[i].x, touches[i].y, 200 * force);
     }
-    // if(mouseIsPressed){
-    //     ellipse(mouseX, mouseY, 200*force);
-    // }
 }
-
-// function handle_touch(event) {
-//     try {
-//         force = event.touches[0].force;
-//     } catch (e) {
-//         console.log("error");
-//     }
-// }
