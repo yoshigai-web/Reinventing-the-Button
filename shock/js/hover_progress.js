6e95b967109a6d48c15ace4a9efb4b1cba38b5f1
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-07-20 08:27:46 +0900

    add hover with progress bar

---
+++ shock/js/hover_progress.js
@@ -0,0 +1,49 @@
+let sound;
+let buttonX = 200, buttonY = 350, buttonWidth = 200, buttonHeihgt = 100;
+let isPressed = false;
+let hoverig = false;
+let time;
+function setup() {
+    sound = loadSound('assets/electric voice.mp3');
+    createCanvas(720, 720);
+    textAlign(CENTER);
+}
+function draw() {
+    background(255);
+    if (isPressed || sound.isPlaying()) {
+        fill(255, 0, 0);
+    } else if (buttonX < mouseX && mouseX < buttonX + buttonWidth && buttonY < mouseY && mouseY < buttonY + buttonHeihgt) {
+        fill(255, 130, 130);
+
+    } else {
+        fill(255);
+    }
+    if (buttonX < mouseX && mouseX < buttonX + buttonWidth && buttonY < mouseY && mouseY < buttonY + buttonHeihgt) {
+        if (!hoverig) {
+            hoverig = true;
+            time = millis();
+        }
+    } else {
+        hoverig = false;
+    }
+    // console.log(hoverig);
+    if (500 < millis() - time && millis() - time < 600 && hoverig) {
+        if (!sound.isPlaying()) sound.play();
+    }
+
+    strokeWeight(3);
+    rect(buttonX, buttonY, buttonWidth, buttonHeihgt, 5);
+    fill(0);
+    if(hoverig && millis()-time<500)rect(buttonX, buttonY+buttonHeihgt, map(millis()-time, 0, 500, 0, buttonWidth), -5);
+}
+function mousePressed() {
+    if (buttonX < mouseX && mouseX < buttonX + buttonWidth && buttonY < mouseY && mouseY < buttonY + buttonHeihgt) {
+        isPressed = true;
+    }
+}
+function mouseReleased() {
+    if (buttonX < mouseX && mouseX < buttonX + buttonWidth && buttonY < mouseY && mouseY < buttonY + buttonHeihgt) {
+        // if (!sound.isPlaying()) sound.play();
+    }
+    isPressed = false;
+}
