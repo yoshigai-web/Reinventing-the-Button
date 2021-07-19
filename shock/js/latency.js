a0277015a61401eebda2193c2c85b5ad1caa0975
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-07-19 21:40:01 +0900

    add latency button

---
+++ shock/js/latency.js
@@ -0,0 +1,37 @@
+let sound;
+let buttonX = 200, buttonY = 350, buttonWidth = 200, buttonHeihgt = 100;
+let isPressed = false;
+let isWating = false;
+let time, latency = Math.random() * (5 - 0.2) + 0.2;
+function setup() {
+    sound = loadSound('assets/ガラスが割れる.mp3');
+    createCanvas(720, 720);
+}
+function draw() {
+    background(255);
+    fill(0);
+    text("Latency = " + latency.toPrecision(3)+" [s]", 100, 100);
+    if (millis() - time > latency * 1000 && isWating) {
+        if (!sound.isPlaying()) sound.play();
+        isWating = false;
+        latency = Math.random() * (5 - 0.2) + 0.2;
+    }
+    if (isPressed) fill(127, 201, 221);
+    else if (sound.isPlaying()) fill(255);
+    else if (buttonX < mouseX && mouseX < buttonX + buttonWidth && buttonY < mouseY && mouseY < buttonY + buttonHeihgt) fill(157, 231, 251);
+    else fill(255);
+    strokeWeight(3);
+    rect(buttonX, buttonY, buttonWidth, buttonHeihgt, 5);
+}
+function mousePressed() {
+    if (buttonX < mouseX && mouseX < buttonX + buttonWidth && buttonY < mouseY && mouseY < buttonY + buttonHeihgt) {
+        isPressed = true;
+    }
+}
+function mouseReleased() {
+    if (buttonX < mouseX && mouseX < buttonX + buttonWidth && buttonY < mouseY && mouseY < buttonY + buttonHeihgt) {
+        time = millis();
+        isWating = true;
+    }
+    isPressed = false;
+}
