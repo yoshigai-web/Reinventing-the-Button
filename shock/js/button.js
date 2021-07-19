b9cd64a2a508d7ed032f2b5fb27ca7cdf8f1464a
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-07-19 20:20:31 +0900

    add shock button

---
+++ shock/js/button.js
@@ -0,0 +1,31 @@
+let sound;
+let buttonX = 200, buttonY = 350, buttonWidth = 200, buttonHeihgt = 100;
+let isPressed = false;
+function setup() {
+    sound = loadSound('assets/ガラスが割れる.mp3');
+    createCanvas(720, 720);
+    textAlign(CENTER);
+}
+function draw() {
+    background(255);
+    if (isPressed) fill(127, 201, 221);
+    else if (sound.isPlaying()) fill(255);
+    else if (buttonX < mouseX && mouseX < buttonX + buttonWidth && buttonY < mouseY && mouseY < buttonY + buttonHeihgt) fill(157, 231, 251);
+    else fill(255);
+    
+    rect(buttonX, buttonY, buttonWidth, buttonHeihgt, 5);
+    fill(0);
+    textSize(20);
+    // text("SHOCK", buttonX + buttonWidth / 2, buttonY + buttonHeihgt / 2 + 8);
+}
+function mousePressed() {
+    if (buttonX < mouseX && mouseX < buttonX + buttonWidth && buttonY < mouseY && mouseY < buttonY + buttonHeihgt) {
+        isPressed = true;
+    }
+}
+function mouseReleased() {
+    if (buttonX < mouseX && mouseX < buttonX + buttonWidth && buttonY < mouseY && mouseY < buttonY + buttonHeihgt) {
+        if (!sound.isPlaying()) sound.play();
+    }
+    isPressed = false;
+}
