e9ba66b569da7719c30908464833fe536363b165
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-08-06 10:28:27 +0900

    Update samePress_with_face.js

--- js/samePress_with_face.js
+++ js/samePress_with_face.js
@@ -1,61 +1,68 @@
-let btnX = 100, btnY = 200, btnW = 100, btnH = 100;
-let btnNum = 5;
-let btn = new Array(btnNum);
+let judgeX = 100, judgeY = 200, judgeW = 100, judgeH = 50;
+let judgeNum = 4;
+let btnX = 390, btnY = 550, btnW = 120, btnH = 80;
+let judge = new Array(judgeNum);
+let btn = false;
 let time;
-let waitTime = new Array(btnNum);
-let maxTime = 10000;
+let waitTime = new Array(judgeNum);
+let maxTime = 2000;
 let pressed = false;
-let img = new Array(btnNum - 1);
+let img = new Array(judgeNum - 1);
 
 function setup() {
     createCanvas(1200, 800);
-    for (let i = 0; i < btnNum - 1; i++)img[i] = loadImage('https://raw.githubusercontent.com/yoshigai-web/Reinventing-the-Button/main/img/face' + i + '.jpeg');
-    for (let i = 0; i < btnNum; i++)waitTime[i] = Math.floor(Math.random() * maxTime);
+    for (let i = 0; i < judgeNum; i++)img[i] = loadImage('https://raw.githubusercontent.com/yoshigai-web/Reinventing-the-Button/main/img/face' + i + '.jpeg');
+    init();
+    textAlign(CENTER, CENTER);
 }
 function draw() {
     background(255);
-
-    for (let i = 0; i < btnNum; i++) {
-        if (btn[i]) {
-            fill(255, 0, 0);
-            if (!pressed) {
-                pressed = true;
-                time = millis();
-            }
-        } else fill(255);
-        rect(btnX + i * 200, btnY, btnW, btnH);
-        if (0 < i) {
-            image(img[i - 1], btnX + i * 200, btnY - 120, btnW, btnH);
-            fill(0);
-            textSize(25);
-            if (btn[0] && !btn[i]) text("Check", btnX + i * 200 + 15, btnY + 50);
-            else if (btn[0] && btn[i]) text("OK", btnX + i * 200 + 15, btnY + 50);
-            else text("Wait", btnX + i * 200 + 15, btnY + 50);
-        } else {
-            fill(255);
-            rect(btnX + i * 200, btnY - 120, btnW, btnH);
-            fill(0);
-            textSize(50);
-            text("me", btnX + i * 200 + 5, btnY - 60);
-        }
+    // draw judge
+    for (let i = 0; i < judgeNum; i++) {
+        if (judge[i]) fill(0, 128, 255, 128);
+        else fill(255);
+        rect(judgeX + i * 200, judgeY, judgeW, judgeH);
+        image(img[i], judgeX + i * 200, judgeY - 120, judgeW, 100)
+        fill(0);
+        textSize(20);
+        if (btn && !judge[i]) text("Check...", judgeX + i * 200 + judgeW / 2, judgeY + judgeH / 2);
+        else if (btn && judge[i]) text("OK!", judgeX + i * 200 + judgeW / 2, judgeY + judgeH / 2);
+        // else text("Wait", judgeX + i * 200 + judgeW / 2, judgeY + judgeH / 2);
     }
+    // draw button
+    if (btn) {
+        fill(255, 0, 0);
+        if (!pressed) {
+            pressed = true;
+            time = millis();
+        }
+    } else if (btnX < mouseX && mouseX < btnX + btnW && btnY < mouseY && mouseY < btnY + btnH) fill(255, 0, 0, 100);
+    else fill(255);
+    rect(btnX, btnY, btnW, btnH, 10);
     if (pressed) {
         let t = millis();
-        for (let i = 0; i < btnNum; i++) if (t - time > waitTime[i]) btn[i] = true;
+        for (let i = 0; i < judgeNum; i++) if (t - time > waitTime[i]) judge[i] = true;
     }
-
-    for (let i = 0; i < btnNum; i++) {
-        if (!btn[i]) break;
+    if (!judge.includes(false)) {
         fill(0);
         textSize(50);
-        if (i == btnNum - 1) text("Done", width / 2, height / 2);
+        text("Done", 450, height / 2);
     }
 }
 function mousePressed() {
-    if (btnX < mouseX && mouseX < btnX + btnW && btnY < mouseY && mouseY < btnY + btnH) btn[0] = !btn[0];
+    if (btnX < mouseX && mouseX < btnX + btnW && btnY < mouseY && mouseY < btnY + btnH) {
+        btn = !btn;
+        if(!btn)init();
+    }
 }
 function keyPressed() {
-    for (let i = 0; i < btnNum; i++) btn[i] = false;
-    for (let i = 0; i < btnNum; i++)waitTime[i] = Math.floor(Math.random() * maxTime);
+    init();
+}
+function init() {
+    for (let i = 0; i < judgeNum; i++) {
+        waitTime[i] = Math.floor(Math.random() * maxTime);
+        judge[i] = false;
+    }
+    btn = false;
     pressed = false;
 }
