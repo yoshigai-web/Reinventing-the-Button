73deb7f7f9f2661eaa851056064cb83bba4dcc49
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-09-21 20:58:38 +0900

    add error in each cursors

--- experiments/js/multi_cursor.js
+++ experiments/js/multi_cursor.js
@@ -3,27 +3,32 @@ let btnNum = 5;
 let btnInterval = 150;
 let btnPressed;
 let cursorImg;
+let cursorErrorX = [], cursorErrorY = [];
 let sound;
 function setup() {
     createCanvas(1200, 800);
     noCursor();
     cursorImg = loadImage('https://raw.githubusercontent.com/yoshigai-web/Reinventing-the-Button/main/img/cursor.png');
     sound = loadSound('assets/electric voice.mp3');
+    for (let i = 0; i < btnNum; i++) {
+        cursorErrorX[i] = int(random(30)) - 15;
+        cursorErrorY[i] = int(random(30)) - 15;
+    }
 }
 function draw() {
     background(255);
     drawButton();
     drawCursor();
     if (btnPressed) {
-        if(int(random(10))==0)sound.play();
+        if (int(random(10)) == 0) sound.play();
         btnPressed = false;
     }
 }
 function mousePressed() {
     // ボタンの上下100ptからカーソルを複数表示する
     if (btnY - 100 < mouseY && mouseY < btnY + btnH + 100) {
-        let x = (mouseX - btnX) % btnInterval;
-        if (x < btnW && btnY < mouseY && mouseY < btnY + btnH) btnPressed = !btnPressed;
+        let relativeMouseX = (mouseX - btnX) % btnInterval;
+        if (relativeMouseX < btnW && btnY < mouseY && mouseY < btnY + btnH) btnPressed = !btnPressed;
     }
 }
 function drawButton() {
@@ -35,9 +40,13 @@ function drawButton() {
 function drawCursor() {
     image(cursorImg, mouseX, mouseY, 286 * 0.08, 429 * 0.08);
     if (btnY - 100 < mouseY && mouseY < btnY + btnH + 100) {
-        let x = (mouseX - btnX) % btnInterval;
+        let relativeMouseX = (mouseX - btnX) % btnInterval;
         for (let i = -1; i <= btnNum; i++) {
-            image(cursorImg, btnX + i * btnInterval + x, mouseY, 286 * 0.08, 429 * 0.08);
+            if (btnX + i * btnInterval + relativeMouseX != mouseX) {
+                let cursorX = btnX + i * btnInterval + relativeMouseX + cursorErrorX[i];
+                let cursorY = mouseY + cursorErrorY[i];
+                image(cursorImg, cursorX, cursorY, 286 * 0.08, 429 * 0.08);
+            }
         }
     }
 }
