8c852ba01755dace740221b32e0f78ea4f97d046
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-09-21 17:09:01 +0900

    bug fixed

--- experiments/js/multi_cursor.js
+++ experiments/js/multi_cursor.js
@@ -1,6 +1,6 @@
 let btnX = 200, btnY = 400, btnW = 100, btnH = 100;
 let btnNum = 5;
-let btnInterval = 200;
+let btnInterval = 170;
 let btnPressed;
 function setup() {
     createCanvas(1200, 800);
@@ -12,23 +12,24 @@ function draw() {
     drawCursor();
 }
 function mousePressed() {
+    // ボタンの上下100ptからカーソルを複数表示する
     if (btnY - 100 < mouseY && mouseY < btnY + btnH + 100) {
-        let x = (mouseX - 100) % btnInterval;
-        if (100 < x && x < 200 && btnY < mouseY && mouseY < btnY + btnH) btnPressed = !btnPressed;
+        let x = (mouseX - btnX) % btnInterval;
+        if (x < btnW && btnY < mouseY && mouseY < btnY + btnH) btnPressed = !btnPressed;
     }
 }
-function drawButton(){
+function drawButton() {
     if (btnPressed) fill(255, 0, 0);
     else fill(255);
     for (let i = 0; i < btnNum; i++)rect(btnX + i * btnInterval, btnY, btnW, btnH);
 }
-function drawCursor(){
+function drawCursor() {
     fill(255);
     ellipse(mouseX, mouseY, 10, 10);
     if (btnY - 100 < mouseY && mouseY < btnY + btnH + 100) {
-        let x = (mouseX - 100) % btnInterval;
-        for (let i = 0; i < btnNum; i++) {
-            ellipse(100 + i * btnInterval + x, mouseY, 10, 10);
+        let x = (mouseX - btnX) % btnInterval;
+        for (let i = -1; i <= btnNum; i++) {
+            ellipse(btnX + i * btnInterval + x, mouseY, 10, 10);
         }
     }
 }
