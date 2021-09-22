67d5d156f99c42d1b655e10c16d20f86dc566c39
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-09-22 09:53:41 +0900

    bug fixed

--- experiments/js/multi_cursor_1.js
+++ experiments/js/multi_cursor_1.js
@@ -3,9 +3,7 @@ let btnNum = 1;
 let btnInterval = 150;
 let btnPressed;
 let cursorImg;
-let cursorErrorX = [], cursorErrorY = [];
 let sound;
-let noiseSeed=0.0;
 function setup() {
     createCanvas(1200, 800);
     noCursor();
@@ -36,21 +34,4 @@ function drawButton() {
 }
 function drawCursor() {
     image(cursorImg, mouseX, mouseY, 286 * 0.08, 429 * 0.08);
-    if (btnY - 100 < mouseY && mouseY < btnY + btnH + 100) {
-        let relativeMouseX = (mouseX - btnX) % btnInterval;
-        for (let i = 0; i < btnNum; i++) {
-            if (btnX + i * btnInterval + relativeMouseX != mouseX) {
-                let cursorX = btnX + i * btnInterval + relativeMouseX + cursorErrorX[i];
-                let cursorY = mouseY + cursorErrorY[i];
-                image(cursorImg, cursorX, cursorY, 286 * 0.08, 429 * 0.08);
-            }
-        }
-    }
-}
-function mouseMoved(){
-    noiseSeed+=0.01;
-    for (let i = -0; i < btnNum; i++) {
-        cursorErrorX[i] = noise((noiseSeed+i*3))*btnW - btnW/2;
-        cursorErrorY[i] = noise((noiseSeed+i*3+1))*btnH - btnH/2;
-    }
 }
