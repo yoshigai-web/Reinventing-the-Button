ef0f2fcab431c857ce429624af644fe838b2b89a
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-09-27 19:13:40 +0900

    add cursor mirroring

---
+++ experiments/js/multi_cursor_mirror.js
@@ -0,0 +1,38 @@
+let btnX, btnY, btnW = 70, btnH = 70;
+let btnPressed = false;
+let cursorImg;
+let sound;
+function setup() {
+    createCanvas(1200, 800);
+    noCursor();
+    cursorImg = loadImage('https://raw.githubusercontent.com/yoshigai-web/Reinventing-the-Button/main/img/cursor.png');
+    sound = loadSound('assets/electric voice.mp3');
+    rectMode(CENTER);
+    btnX = width / 2, btnY = height / 2;
+}
+function draw() {
+    background(255);
+    drawButton();
+    drawCursor();
+    if (btnPressed) {
+        if (int(random(10)) == 0) sound.play();
+        btnPressed = false;
+    }
+}
+function drawButton() {
+    if (btnPressed) fill(255, 0, 0);
+    else if (btnX - 100 < mouseX && mouseX < btnX + btnW + 100 && btnY - 100 < mouseY && mouseY < btnY + btnH + 100) fill(255, 100, 100, 100);
+    else fill(255);
+    rect(btnX, btnY, btnW, btnH);
+}
+function drawCursor() {
+    image(cursorImg, mouseX, mouseY, 286 * 0.08, 429 * 0.08);
+    image(cursorImg, width - mouseX, mouseY, 286 * 0.08, 429 * 0.08);
+    image(cursorImg, mouseX, height - mouseY, 286 * 0.08, 429 * 0.08);
+    image(cursorImg, width - mouseX, height - mouseY, 286 * 0.08, 429 * 0.08);
+}
+function mousePressed() {
+    if (btnX - btnW / 2 < mouseX && mouseX < btnX + btnW / 2 && btnY - btnH / 2 < mouseY && mouseY < btnY + btnH) {
+        btnPressed = !btnPressed;
+    }
+}
