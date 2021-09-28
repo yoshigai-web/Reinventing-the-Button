a4f2194491553efe3d0f2c200428cf93523c240b
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-09-28 15:26:27 +0900

    make it beautiful

--- experiments/js/multi_cursor_order_first.js
+++ experiments/js/multi_cursor_order_first.js
@@ -1,11 +1,10 @@
-let btnX = 100, btnY = 200, btnW = 100, btnH = 100;
+let btnX = 100, btnY = 200, btnW = 100, btnH = 100, btnInterval = 200;
 let btnNum = 5;
-let btn = new Array(btnNum);
-let time;
-let waitTime = new Array(btnNum);
+let btnPressed = [];
+let myBtn;
+let pressedTime;
+let waitTime = [];
 let maxTime = 1500;
-let pressed = false;
-let first;
 let img;
 function setup() {
     createCanvas(1200, 800);
@@ -15,39 +14,23 @@ function setup() {
 }
 function draw() {
     background(255);
-    for (let i = 0; i < btnNum; i++) {
-        if (btn[i]) {
-            fill(255, 0, 0);
-            if (!pressed) {
-                pressed = true;
-                time = millis();
-            }
-        } else if (btnX + i * 200 < mouseX && mouseX < btnX + i * 200 + btnW && btnY < mouseY && mouseY < btnY + btnH) fill(255, 0, 0, 100);
-        else fill(255);
-        rect(btnX + i * 200, btnY, btnW, btnH, 10);
-        if (!btn[i] && pressed) image(img, btnX + i * 200 + noise(i) * btnW, btnY + noise(i) * btnH + waitTime[i] - (millis() - time), 286 * 0.08, 429 * 0.08);
-        if (btn[i] && first != i) image(img, btnX + i * 200 + noise(i) * btnW, btnY + noise(i) * btnH, 286 * 0.08, 429 * 0.08);
-    }
-    if (pressed) {
-        let t = millis();
-        for (let i = 0; i < btnNum; i++) if (t - time > waitTime[i]) btn[i] = true;
-    }
-    if (!btn.includes(false)) {
-        fill(0);
-        textSize(50);
-        text("Done", 480, height / 2);
-    }
-    image(img, mouseX, mouseY, 286 * 0.08, 429 * 0.08);
+    drawButton();
+    drawCursor();
+    makeBtnPressed();
+    checkFinished();
+    image(img, mouseX, mouseY, 286 * 0.08, 429 * 0.08); // draw my cursor
 }
 function mousePressed() {
-    if (!btn.includes(false)) {
+    if (!btnPressed.includes(false)) {  // if all buttons are pressed
         init();
         return;
     }
+    // check button pressed
     for (let i = 0; i < btnNum; i++) {
-        if (btnX + i * 200 < mouseX && mouseX < btnX + i * 200 + btnW && btnY < mouseY && mouseY < btnY + btnH) {
-            btn[i] = !btn[i];
-            first = i;
+        if (btnX + i * btnInterval < mouseX && mouseX < btnX + i * btnInterval + btnW && btnY < mouseY && mouseY < btnY + btnH) {
+            btnPressed[i] = !btnPressed[i];
+            myBtn = i;
+            pressedTime = millis();
         }
     }
 }
@@ -55,7 +38,47 @@ function keyPressed() {
     init();
 }
 function init() {
-    for (let i = 0; i < btnNum; i++) btn[i] = false;
-    for (let i = 0; i < btnNum; i++)waitTime[i] = Math.floor(Math.random() * maxTime);
-    pressed = false;
+    for (let i = 0; i < btnNum; i++) btnPressed[i] = false;
+    for (let i = 0; i < btnNum; i++) waitTime[i] = Math.floor(Math.random() * maxTime);
+}
+function drawButton() {
+    for (let i = 0; i < btnNum; i++) {
+        if (btnPressed[i]) {    // already pressed
+            fill(255, 0, 0);
+        } else if (btnX + i * btnInterval < mouseX && mouseX < btnX + i * btnInterval + btnW && btnY < mouseY && mouseY < btnY + btnH) {    // hover
+            fill(255, 0, 0, 100);
+        } else {    // nothing
+            fill(255);
+        }
+        rect(btnX + i * btnInterval, btnY, btnW, btnH, 10);
+    }
+}
+function drawCursor() {
+    for (let i = 0; i < btnNum; i++) {
+        // moving
+        if (!btnPressed[i] && btnPressed.includes(true)) {
+            image(img, btnX + i * btnInterval + noise(i) * btnW, btnY + noise(i) * btnH + waitTime[i] - (millis() - pressedTime), 286 * 0.08, 429 * 0.08);
+        }
+        // last position
+        if (btnPressed[i] && myBtn != i) {
+            image(img, btnX + i * btnInterval + noise(i) * btnW, btnY + noise(i) * btnH, 286 * 0.08, 429 * 0.08);
+        }
+    }
+}
+function makeBtnPressed() {
+    if (btnPressed.includes(true)) {
+        let t = millis();
+        for (let i = 0; i < btnNum; i++) {
+            if (t - pressedTime > waitTime[i]) {
+                btnPressed[i] = true;
+            }
+        }
+    }
+}
+function checkFinished() {
+    if (!btnPressed.includes(false)) {
+        fill(0);
+        textSize(50);
+        text("Done", 480, height / 2);
+    }
 }
