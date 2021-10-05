fc2b53062d09d6669c1e86c2c799bc75f73cb965
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-10-05 17:16:14 +0900

    add random push

--- experiments/js/multi_cursor_roulette.js
+++ experiments/js/multi_cursor_roulette.js
@@ -3,6 +3,7 @@ let btnPressed = false;
 let cursorImg;
 let cursorNum = 5;
 let cursorRotation = 0;
+let roulette = [], rouletteNum;
 let sound;
 let pressedTime;
 function setup() {
@@ -11,6 +12,9 @@ function setup() {
     cursorImg = loadImage('https://raw.githubusercontent.com/yoshigai-web/Reinventing-the-Button/main/img/cursor.png');
     sound = loadSound('assets/electric voice.mp3');
     btnX = width / 2, btnY = height / 2;
+    for (let i = 0; i < cursorNum; i++) roulette[i] = i;
+    roulette = shuffle(roulette);
+    rouletteNum = int(random(1, cursorNum - 1));
 }
 function draw() {
     background(255);
@@ -32,12 +36,21 @@ function drawButton() {
 function drawCursor() {
     if (btnX < mouseX && mouseX < btnX + btnW && btnY < mouseY && mouseY < btnY + btnH) {
         push();
-        translate(mouseX, mouseY-10);
+        translate(mouseX, mouseY - 10);
 
         rotate(cursorRotation += 0.01);
         for (let i = 0; i < cursorNum; i++) {
-            rotate(2 * PI / cursorNum);
-            image(cursorImg, 0, 10 - btnPressed * 8, 286 * 0.08, 429 * 0.08);
+            push();
+            rotate(2 * PI / cursorNum * roulette[i]);
+            if (rouletteNum < i) {
+                if (btnPressed) image(cursorImg, 0, 10 - 8, 286 * 0.06, 429 * 0.06);
+                else image(cursorImg, 0, 10, 286 * 0.08, 429 * 0.08);
+                // image(cursorImg, 0, 10 - btnPressed * 8, 286 * 0.05, 429 * 0.05);
+            } else {
+                image(cursorImg, 0, 10, 286 * 0.08, 429 * 0.08);
+            }
+
+            pop();
         }
         pop();
     } else {
@@ -51,5 +64,15 @@ function mousePressed() {
     if (btnX < mouseX && mouseX < btnX + btnW && btnY < mouseY && mouseY < btnY + btnH) {
         btnPressed = !btnPressed;
         pressedTime = millis();
+        roulette = shuffle(roulette);
+        rouletteNum = int(random(1, cursorNum - 1));
+        print(rouletteNum, roulette)
     }
+}
+const shuffle = ([...array]) => {
+    for (let i = array.length - 1; i >= 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
