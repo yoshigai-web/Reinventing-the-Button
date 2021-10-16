df6a4eafdac2af0a89849ad7f3b06c02bc19615e
yoshigai-web <85503098+yoshigai-web@users.noreply.github.com>
2021-10-16 12:34:37 +0900

    keep fingers pressed positioin

--- experiments/js/mobile_finger_kokkuri-san.js
+++ experiments/js/mobile_finger_kokkuri-san.js
@@ -4,10 +4,11 @@ let cursorImg, fingerImg;
 let cursorNum = 5;
 let cursorRotation = 0;
 let cursorDistance = -10;
-let pressedX, pressedY;
 let sound;
 let pressedTime;
 let sel;
+let touchX, touchY;
+let releasedX, releasedY;
 function setup() {
     createCanvas(windowWidth, windowHeight);
     noCursor();
@@ -35,12 +36,21 @@ function draw() {
             btnPressed = false;
         } else {
             if (int(random(10 * 30)) == 0) sound.play();
+            push();
+            translate(releasedX, releasedY - cursorDistance);
+            for (let i = 0; i < cursorNum - 1; i++) {
+                push();
+                rotate(2 * PI / cursorNum * (i + 1));
+                image(fingerImg, -335, cursorDistance, 284 * 2.5, 497 * 2.5);
+                pop();
+            }
+            pop();
         }
     }
 }
 function drawButton() {
     if (btnPressed) fill(255, 0, 0);
-    else if (btnX < mouseX && mouseX < btnX + btnW && btnY < mouseY && mouseY < btnY + btnH) fill(255, 100, 100, 50);
+    else if (btnX < touchX && touchX < btnX + btnW && btnY < touchY && touchY < btnY + btnH) fill(255, 100, 100, 50);
     else fill(255);
     strokeWeight(5);
     rect(btnX, btnY, btnW, btnH, 10);
@@ -59,14 +69,16 @@ function drawFinger() {
         pop();
     }
 }
-function touchStarted() {
+function touchMoved() {
     if (touches.length > 0) {
-        let touchX = touches[0].x, touchY = touches[0].y;
-        if (btnX < touchX && touchX < btnX + btnW && btnY < touchY && touchY < btnY + btnH && !btnPressed) {
-            btnPressed = true;
-            pressedTime = millis();
-            pressedX = touchX, pressedY = touchY;
-        }
+        touchX = touches[0].x, touchY = touches[0].y;
+    }
+}
+function touchEnded() {
+    if (btnX < touchX && touchX < btnX + btnW && btnY < touchY && touchY < btnY + btnH && !btnPressed) {
+        btnPressed = true;
+        pressedTime = millis();
+        releasedX = touchX, releasedY = touchY;
     }
 }
 const shuffle = ([...array]) => {
